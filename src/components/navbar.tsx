6e5058fa87955609d9a91a96e931d9c47e3e3960
Yanatchara JERAJA <yanatchara.j@ku.th>
2024-08-17 11:58:22 +0700

    update router

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,14 +1,21 @@
 "use client";
 
 import React from "react";
+import { useRouter } from "next/navigation";
 
-export default function Navbar({}) {
+
+export default function Navbar() {
+  const router = useRouter();
+  const handleLogin = () => {
+    router.push("/login");
+  }
+
   return (
     <div className="w-full h-[90px] bg-white shadow-md flex px-[102px] items-center">
       <div className="text-3xl font-bold">B2D</div>
       <div
-        className="text-2xl text-base ml-auto"
-        onClick={() => console.log("log in")}
+        className="text-2xl text-base ml-auto hover:cursor-pointer"
+        onClick={handleLogin}
       >
         Log in
       </div>
