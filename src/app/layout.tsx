0b83c23c5bbe4a79c89dbf6373f6e816a5df1982
Yanatchara JERAJA <yanatchara.j@ku.th>
2024-08-17 13:26:19 +0700

    intit layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from "next";
-import { Prompt } from "next/font/google";
+import { Kumbh_Sans } from "next/font/google";
 import "@/styles/globals.css";
 
 import Navbar from "@/components/navbar";
 
-const prompt = Prompt({
+const prompt = Kumbh_Sans({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  subsets: ["latin", "thai"],
+  subsets: ["latin", "latin-ext"],
 });
 
 export const metadata: Metadata = {
