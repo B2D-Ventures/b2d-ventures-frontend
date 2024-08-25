a1f353b09b9cba548785a7e61158d7977475b58d
Yanatchara JERAJA <yanatchara.j@ku.th>
2024-08-25 13:37:33 +0700

    update function

--- src/app/example/selection-role/page.tsx
+++ src/app/example/selection-role/page.tsx
@@ -1,34 +1,52 @@
-import React from 'react';
+import React from "react";
 const HomePage = () => {
+
+  // const createUser = () => {
+  //   fetch
+  // }
+
+  // const handleInvestor = () => {};
 
   return (
     <div>
-    <div
-  className="flex h-screen items-center justify-center"
-  style={{background: 'linear-gradient(to right, #FFFFFF 50%, #4E0A81 50%)'}}>
-    <div className="bg-white rounded-lg shadow-lg border-2 border-[#D9D9D9]" style={{ width: '1288px', height: '631px' }}>
-      <div className="flex h-full">
-        <div className="flex-1 bg-white text-center mt-12 mb-12">
-          <div className="inline-block space-x-2 mb-24">
-          <p className="text-[#625B71] text-4xl font-bold inline">Please select</p>
-          <p className="text-[#000000] text-4xl font-bold inline">your Role</p>
-          </div>
-          <div className="flex-col items-center justify-center">
-          <div className='my-12'>
-            <button className="bg-[#FFFFFF] text-[#000000] py-2 px-6 rounded-xl border-2 border-[#D9D9D9] shadow-md hover:shadow-lg w-1/5 font-medium">
-            INVESTOR
-            </button>
-          </div>
-          <div className='my-12'>
-            <button className="bg-[#FFFFFF] text-[#000000] py-2 px-6 rounded-xl border-2 border-[#D9D9D9] shadow-md hover:shadow-lg w-1/5 font-medium">
-            STARTUP
-            </button>
-          </div>
+      <div
+        className="flex h-screen items-center justify-center"
+        style={{
+          background: "linear-gradient(to right, #FFFFFF 50%, #4E0A81 50%)",
+        }}
+      > 
+        <div
+          className="bg-white rounded-lg shadow-lg border-2 border-[#D9D9D9]"
+          style={{ width: "1288px", height: "631px" }}
+        >
+          <div className="flex h-full">
+            <div className="flex-1 bg-white text-center mt-12 mb-12">
+              <div className="inline-block space-x-2 mb-24">
+                <p className="text-[#625B71] text-4xl font-bold inline">
+                  Please select
+                </p>
+                <p className="text-[#000000] text-4xl font-bold inline">
+                  your Role
+                </p>
+              </div>
+              <div className="flex-col items-center justify-center">
+                <div className="my-12">
+                  <button className="bg-[#FFFFFF] text-[#000000] py-2 px-6 rounded-xl border-2 border-[#D9D9D9] shadow-md hover:shadow-lg w-1/5 font-medium"
+                  // onClick={handleInvestor}
+                  >
+                    INVESTOR
+                  </button>
+                </div>
+                <div className="my-12">
+                  <button className="bg-[#FFFFFF] text-[#000000] py-2 px-6 rounded-xl border-2 border-[#D9D9D9] shadow-md hover:shadow-lg w-1/5 font-medium">
+                    STARTUP
+                  </button>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </div>
     </div>
   );
 };
