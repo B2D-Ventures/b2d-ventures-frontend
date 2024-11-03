49ec0ed0255c197b36fb87b24e6bd6dae22dd3ec
Yanatchara JERAJA <yanatchara.j@ku.th>
2024-11-03 12:57:05 +0700

    admin-manage-deal

--- cypress/e2e/admin-manage-deal.cy.ts
+++ cypress/e2e/admin-manage-deal.cy.ts
@@ -1,5 +1,9 @@
 describe("template spec", () => {
+  it("Create deal form", () => {
     cy.visit("http://localhost:3000/admin-management");
-    cy.get(".text-2xl");
     cy.wait(3000);
+    cy.get('[data-testid="deal-accordion"]').eq(0).should("be.visible");
+    cy.get('[data-testid="deal-accordion"]').eq(0).click();
+    cy.wait(1000);
+  });
 });
--- src/components/AccordainForAdmin.tsx
+++ src/components/AccordainForAdmin.tsx
@@ -1,26 +1,29 @@
 "use client";
 import React from "react";
 import Image from "next/image";
-import { Accordion, AccordionItem } from "@nextui-org/react";
+import {
+  Accordion,
+  AccordionItem,
+  Button,
+  ButtonGroup,
+} from "@nextui-org/react";
+import axios from "axios";
+
+interface AccordainForAdminProps {
+  deals: any[];
+}
+
+interface Deal {
+  attributes: {
+    investor_count: number;
+  };
+}
 
 const API_BASE_URL = "http://127.0.0.1:8000";
 const DEFAULT_BG_IMAGE = "/images/lexi.png"; // Replace with your default background image path
 const DEFAULT_ICON_IMAGE = "/images/icon.jpg"; // Replace with your default icon image path
-interface Deal {
-  attributes: {
-    id: string;
-    name: string;
-    description: string;
-    allocation: number;
-    raised: string;
-    investor_count: number;
-    image_logo_url: string;
-    image_content_url: string;
-    type: string;
-  };
-}
 
-export default function App({ deals }: { deals: Deal[] }) {
+export default function App({ deals }: AccordainForAdminProps) {
   const getImageSrc = (imagePath?: string, isIcon: boolean = false) => {
     if (!imagePath) return isIcon ? DEFAULT_ICON_IMAGE : DEFAULT_BG_IMAGE;
     const fullPath = imagePath.startsWith("http")
@@ -30,13 +33,37 @@ export default function App({ deals }: { deals: Deal[] }) {
   };
 
   function numberToStringFormat(amount: number) {
-    return amount? amount > 999999 ? `$${(amount / 1000000).toLocaleString()}M` : amount > 999 ? `$${(amount / 1000).toLocaleString()}K` : amount : 0;
+    return amount
+      ? amount > 999999
+        ? `$${(amount / 1000000).toLocaleString()}M`
+        : amount > 999
+        ? `$${(amount / 1000).toLocaleString()}K`
+        : amount
+      : 0;
+  }
+
+  function formatDate(isoDate: string) {
+    const date = new Date(isoDate);
+    const options = { year: 'numeric', month: 'short', day: 'numeric' };
+    return date.toLocaleDateString();
+  }
+
+  const handleRemove = async ({dealID}: {dealID: String}) => {
+    try {
+      const response = await axios.get<{ data: any }>(
+        `http://127.0.0.1:8000/api/admin/${dealID}/deals/`
+      );
+      console.log("Dashboard fetched:", response.data);
+    } catch (error) {
+      console.error("Error fetching deals:", error);
+    }
   }
 
   return (
     <Accordion>
       {deals.map((deal) => (
         <AccordionItem
+          data-testid="deal-accordion"
           key={deal.attributes.id}
           aria-label="Accordion 1"
           title={deal.attributes.name}
@@ -55,17 +82,35 @@ export default function App({ deals }: { deals: Deal[] }) {
               <div className="text-[20px] font-bold">
                 {deal.attributes.investor_count}
               </div>
-              <div className="text-secondary">Investment number</div>
+              <div className="text-secondary">investors</div>
             </div>
             <div className="flex flex-col">
-              <div className="text-[20px] font-bold">
-                ${deal.attributes.raised}
-              </div>
+              <div className="text-[20px] font-bold">{numberToStringFormat(deal.attributes.raised)}</div>
               <div className="text-secondary">raised</div>
             </div>
             <div className="flex flex-col">
               <div className="text-[20px] font-bold">{numberToStringFormat(deal.attributes.allocation)}</div>
               <div className="text-secondary">Funding goal</div>
+            </div>
+            <div className="flex flex-col">
+              <div className="text-[20px]">{formatDate(deal.attributes.end_date)}</div>
+              <div className="text-secondary">Deal end</div>
+            </div>
+            <div className="flex flex-cols-2 justify-between mt-2">
+              <div className="flex flex-col">
+                <Button
+                  style={{ borderColor: "#FF0000", color: "#FF0000" }}
+                  variant="bordered"
+                  onClick={() => handleRemove(deal.id)}
+                >
+                  Remove
+                </Button>
+              </div>
+              {/* <div className="flex flex-col">
+                <Button style={{ backgroundColor: "#9710FF", color: "#fff" }}>
+                  End Funding
+                </Button>
+              </div> */}
             </div>
           </div>
         </AccordionItem>
