00a1b4e7ad7e6b90b49da207f7fc12d81a89fcd8
Yanatchara JERAJA <yanatchara.j@ku.th>
2024-11-03 11:44:49 +0700

    admin-management

--- src/components/AccordainForAdmin.tsx
+++ src/components/AccordainForAdmin.tsx
@@ -1,144 +1,119 @@
 "use client";
 import React from "react";
 import Image from "next/image";
-import { Accordion, AccordionItem, Button, ButtonGroup } from "@nextui-org/react";
+import {
+  Accordion,
+  AccordionItem,
+  Button,
+  ButtonGroup,
+} from "@nextui-org/react";
+import axios from "axios";
 
-export default function App() {
-  const defaultContent =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+interface AccordainForAdminProps {
+  deals: any[];
+}
 
-  const content = (
-    <div className="grid grid-cols-3 pl-12">
-      <div className="flex flex-col">
-        <div className="text-[20px] font-bold">13</div>
-        <div className="text-secondary">investors</div>
-      </div>
-      <div className="flex flex-col">
-        <div className="text-[20px] font-bold">$12,390</div>
-        <div className="text-secondary">raised</div>
-      </div>
-      <div className="flex flex-col">
-        <div className="text-[20px] font-bold">$1.2M</div>
-        <div className="text-secondary">Funding goal</div>
-      </div>
-        <div className="flex flex-col">
-            <div className="text-[20px]">Aug 6, 2024</div>
-            <div className="text-secondary">Deal end</div>
-        </div>
-        <div className="flex flex-cols-2 justify-between mt-2">
-        <div className="flex flex-col">
-            <Button style={{ borderColor: '#FF0000', color: '#FF0000' }} variant="bordered">
-                Remove
-            </Button>  
-        </div>
-        <div className="flex flex-col">
-            <Button style={{ backgroundColor: '#9710FF', color: '#fff' }}>
-                End Funding
-            </Button>
-        </div>
-      </div>
-    </div>
+interface Deal {
+  attributes: {
+    investor_count: number;
+  };
+}
 
-  );
+const API_BASE_URL = "http://127.0.0.1:8000";
+const DEFAULT_BG_IMAGE = "/images/lexi.png"; // Replace with your default background image path
+const DEFAULT_ICON_IMAGE = "/images/icon.jpg"; // Replace with your default icon image path
+
+export default function App({ deals }: AccordainForAdminProps) {
+  const getImageSrc = (imagePath?: string, isIcon: boolean = false) => {
+    if (!imagePath) return isIcon ? DEFAULT_ICON_IMAGE : DEFAULT_BG_IMAGE;
+    const fullPath = imagePath.startsWith("http")
+      ? imagePath
+      : `${API_BASE_URL}${imagePath}`;
+    return fullPath;
+  };
+
+  function numberToStringFormat(amount: number) {
+    return amount
+      ? amount > 999999
+        ? `$${(amount / 1000000).toLocaleString()}M`
+        : amount > 999
+        ? `$${(amount / 1000).toLocaleString()}K`
+        : amount
+      : 0;
+  }
+
+  function formatDate(isoDate: string) {
+    const date = new Date(isoDate);
+    const options = { year: 'numeric', month: 'short', day: 'numeric' };
+    return date.toLocaleDateString();
+  }
+
+  const handleRemove = async ({dealID}: {dealID: String}) => {
+    try {
+      const response = await axios.get<{ data: any }>(
+        `http://127.0.0.1:8000/api/admin/${dealID}/deals/`
+      );
+      console.log("Dashboard fetched:", response.data);
+    } catch (error) {
+      console.error("Error fetching deals:", error);
+    }
+  }
 
   return (
     <Accordion>
-      <AccordionItem
-        key="1"
-        aria-label="Accordion 1"
-        title="LEXI"
-        startContent={
-          <Image
-            src="/images/icon.jpg"
-            width={48}
-            height={48}
-            alt="logo"
-            className="w-[40px] h-[40px] rounded-[8px] overflow-hidden shadow-sm"
-          />
-        }
-      >
-        {content}
-      </AccordionItem>
-      <AccordionItem
-        key="2"
-        aria-label="Accordion 2"
-        title="Angrybird"
-        startContent={
-          <Image
-            src="/images/icon.jpg"
-            width={48}
-            height={48}
-            alt="logo"
-            className="w-[40px] h-[40px] rounded-[8px] overflow-hidden shadow-sm"
-          />
-        }
-      >
-        {defaultContent}
-      </AccordionItem>
-      <AccordionItem
-        key="3"
-        aria-label="Accordion 3"
-        title="Jojo 333333"
-        startContent={
-          <Image
-            src="/images/icon.jpg"
-            width={48}
-            height={48}
-            alt="logo"
-            className="w-[40px] h-[40px] rounded-[8px] overflow-hidden shadow-sm"
-          />
-        }
-      >
-        {defaultContent}
-      </AccordionItem>
-      <AccordionItem
-        key="4"
-        aria-label="Accordion 4"
-        title="jj"
-        startContent={
-          <Image
-            src="/images/icon.jpg"
-            width={48}
-            height={48}
-            alt="logo"
-            className="w-[40px] h-[40px] rounded-[8px] overflow-hidden shadow-sm"
-          />
-        }
-      >
-        {defaultContent}
-      </AccordionItem>
-      <AccordionItem
-        key="5"
-        aria-label="Accordion 5"
-        title="koko"
-        startContent={
-          <Image
-            src="/images/icon.jpg"
-            width={48}
-            height={48}
-            alt="logo"
-            className="w-[40px] h-[40px] rounded-[8px] overflow-hidden shadow-sm"
-          />
-        }
-      >
-        {defaultContent}
-      </AccordionItem>
-      <AccordionItem
-        key="6"
-        aria-label="Accordion 6"
-        title="lolo"
-        startContent={
-          <Image
-            src="/images/icon.jpg"
-            width={48}
-            height={48}
-            alt="logo"
-            className="w-[40px] h-[40px] rounded-[8px] overflow-hidden shadow-sm"
-          />
-        }
-      >
-        {defaultContent}
-      </AccordionItem>
+      {deals.map((deal) => (
+        <AccordionItem
+          key={deal.attributes.id}
+          aria-label="Accordion 1"
+          title={deal.attributes.name}
+          startContent={
+            <Image
+              src={getImageSrc(deal.attributes.image_logo_url)}
+              width={48}
+              height={48}
+              alt="logo"
+              className="w-[40px] h-[40px] rounded-[8px] overflow-hidden shadow-sm"
+            />
+          }
+        >
+          <div className="grid grid-cols-3 pl-12">
+            <div className="flex flex-col">
+              <div className="text-[20px] font-bold">
+                {deal.attributes.investor_count}
+              </div>
+              <div className="text-secondary">investors</div>
+            </div>
+            <div className="flex flex-col">
+              <div className="text-[20px] font-bold">{numberToStringFormat(deal.attributes.raised)}</div>
+              <div className="text-secondary">raised</div>
+            </div>
+            <div className="flex flex-col">
+              <div className="text-[20px] font-bold">{numberToStringFormat(deal.attributes.allocation)}</div>
+              <div className="text-secondary">Funding goal</div>
+            </div>
+            <div className="flex flex-col">
+              <div className="text-[20px]">{formatDate(deal.attributes.end_date)}</div>
+              <div className="text-secondary">Deal end</div>
+            </div>
+            <div className="flex flex-cols-2 justify-between mt-2">
+              <div className="flex flex-col">
+                <Button
+                  style={{ borderColor: "#FF0000", color: "#FF0000" }}
+                  variant="bordered"
+                  onClick={() => handleRemove(deal.id)}
+                >
+                  Remove
+                </Button>
+              </div>
+              {/* <div className="flex flex-col">
+                <Button style={{ backgroundColor: "#9710FF", color: "#fff" }}>
+                  End Funding
+                </Button>
+              </div> */}
+            </div>
+          </div>
+        </AccordionItem>
+      ))}
     </Accordion>
   );
 }
