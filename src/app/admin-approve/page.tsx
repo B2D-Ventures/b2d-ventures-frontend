d79c452c86699b1e09074207fa1cfefcb8d88ae9
Yanatchara JERAJA <yanatchara.j@ku.th>
2024-11-06 14:48:33 +0700

    admin-approve

--- src/app/admin-approve/page.tsx
+++ src/app/admin-approve/page.tsx
@@ -1,30 +1,33 @@
-import DealTable from '@/components/DealTable';
-import SearchBar from "@/components/searchBar";
+"use client";
+import DealTable from "@/components/DealTable";
 import { Button } from "@nextui-org/react";
-import {Pagination, PaginationItem, PaginationCursor} from "@nextui-org/pagination";
+import { useRouter } from "next/navigation";
 
 const Home: React.FC = () => {
-  
+  const router = useRouter();
+  const handleClick = () => {
+    router.push("/admin-management");
+  };
+
   return (
     <div className="container mx-auto p-10">
-    <div className="flex justify-between items-center">
+      <div className="flex justify-between items-center">
         <div>
-          <h1 className="text-4xl font-bold">Admin Dashboard</h1>
+          <h1 className="text-4xl font-bold">Admin Approve Dashboard</h1>
           <p className="text-lg text-black-600 mt-2">Approve startup deals.</p>
         </div>
         <div className="flex items-center space-x-4">
-            <Button style={{ backgroundColor: '#9710FF', color: '#fff' }}>
-                Manage Startup
-            </Button>
-            {/* <SearchBar /> */}
+          <Button
+            onClick={handleClick}
+            style={{ backgroundColor: "#9710FF", color: "#fff" }}
+          >
+            Manage Startup
+          </Button>
         </div>
-    </div>
-    <div className="mt-4">
+      </div>
+      <div className="mt-4">
         <DealTable />
-    </div>
-    <div className="flex justify-center mt-4">
-        <Pagination showControls total={10} initialPage={1} />
-    </div>
+      </div>
     </div>
   );
 };
